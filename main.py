f03ae83815ad070affcdce275608d7b4ff179f03
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2022-01-18 19:19:35 +0300

    Change vocal logic and add some documentation

--- main.py
+++ main.py
@@ -52,12 +52,15 @@ def GetGlobalMinimum(func, interval, e, var=sym.Symbol('x')):
 
 
 def RunTest(test, vocal=None):
-    '''
+    """
+    Принимает тест в формате
+    expression = выражение; interval = [левый конец, правый конец]; e = число; expected = число
+    И запускает GetCriticalPoints для этого теста, после чего проверяет, что expected попало в множество критических точек
 
-    :param test:
-    :param vocal:
-    :return:
-    '''
+    У vocal 3 возможных значения: None, True и False
+    В режиме True функция печатает поданный ей на вход тест и результат вычисления
+    В остальных режимах функция ничего не печатает
+    """
 
     test_format = r"expression = ([^;]+); interval = \[([^;,]+), ([^;,]+)\]; e = ([^;]+); expected = ([^;]+)"
     m = re.match(test_format, test)
@@ -67,28 +70,36 @@ def RunTest(test, vocal=None):
     expected = float(m.group(5))
 
     critical_points = GetCriticalPoints(expression, interval, e)
-    if vocal is not None:
+    if vocal:
         print(f"Expression = {expression}, interval = {interval}, e = {e}")
         print("Critical points are ", end='')
         print_as_points(critical_points)
 
-        if vocal:
-            print(f"Intervals are {critical_points}")
+        print(f"Intervals are {critical_points}")
         print(f"Expected {expected}")
 
     if critical_points.is_in(expected):
-        if vocal is not None:
+        if vocal:
             print_green("Passed!")
             print()
         return True
     else:
-        if vocal is not None:
+        if vocal:
             print_red(f"Failed!")
             print()
         return False
 
 
 def RunTests(file='tests.txt', vocal=None):
+    """
+    Открывает файл file и читает из него тесты в формате
+    expression = выражение; interval = [левый конец, правый конец]; e = число; expected = число
+
+    У vocal 3 возможных значения: None, True и False
+    В режиме None функция ничего не печатает и выбрасывает AssertionError в случае провала тестов
+    В режиме False функция печатает результат тестов
+    В режиме True функция печатает каждый тест в отдельности и итоговый результат
+    """
     with open(file, 'r') as f:
         tests = f.readlines()
         tests_finished = 0
@@ -99,23 +110,30 @@ def RunTests(file='tests.txt', vocal=None):
                 continue
 
             try:
-                if vocal is not None:
+                if vocal:
                     print_yellow(f"Running test {tests_finished}")
                 result = RunTest(line, vocal)
                 if not result:
                     tests_not_passed += 1
             except (ValueError, TypeError, AttributeError):
                 tests_fail_to_match += 1
-                if vocal is not None:
+                if vocal:
                     print_red(f"Failed to match test {tests_finished}")
                 raise
             tests_finished += 1
+
+
+        tests_passed = tests_finished - tests_fail_to_match - tests_not_passed
+        if vocal is None:
+            if tests_passed != tests_finished:
+                raise AssertionError(f"Tests not passed: failed to match {tests_fail_to_match}, failed {tests_not_passed}")
+            else:
+                return
 
         print_yellow(f"Run {tests_finished} tests")
         if tests_fail_to_match != 0:
             print_red(f"Failed to match {tests_fail_to_match} tests")
 
-        tests_passed = tests_finished - tests_fail_to_match - tests_not_passed
         print_green(f"Passed {tests_passed}")
         if tests_passed != tests_finished:
             print_red(f"Failed {tests_not_passed}")
